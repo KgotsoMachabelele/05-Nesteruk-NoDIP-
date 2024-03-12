9334b2a3cfdd9939c98676a1f125f4bd64db2d60
CCGSYSTEMS <CCGSYSTEMS@CCG>
2024-03-12 08:21:34 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+//This is high level
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Define persons
+        var parent = new Person { Name = "John" };
+        var child1 = new Person { Name = "Chris" };
+        var child2 = new Person { Name = "Mary" };
+
+        //Add relationships between people
+        var relationships = new Relationships();
+        relationships.AddParentAndChild(parent, child1);
+        relationships.AddParentAndChild(parent, child2);
+
+        //List relationships
+        ListRelationships(relationships);
+
+        Console.ReadKey();
+    } //Main
+
+    private static void ListRelationships(Relationships relationships)
+    {
+        //Find all of John's children through accessing the low-level Relations property
+        var relations = relationships.Relations;
+        foreach (var r in relations.Where(   x => x.Item1.Name == "John"
+                                          && x.Item2 == Relationship.Parent))
+            Console.WriteLine($"John has a child called {r.Item3.Name}");
+    } //ListRelationships
+
+} //class Program
+
---
+++ Relationships.cs
@@ -0,0 +1,40 @@
+﻿//Nesteruk, Video 7
+//Scenario: Genealogy application
+
+//Relationships between people
+using System.Collections.Generic;
+
+//Possible relationships between people
+public enum Relationship
+{
+    Parent,
+    Child,
+    Sibling
+} //enum Relationships
+
+//Low level
+public class Relationships
+{
+    //Private list
+    //- Use the C#7 tuples
+    //  Person from which, relationshiop, applies to person
+    private List<(Person, Relationship, Person)> relations = new List<(Person, Relationship, Person)>();
+
+    //Property getter of private list
+    //- Nesteruk: "Now the problem with this scenario and the reason why the dependency inversion 
+    //             principle exists is that's we are accessing a very low level part of the relationships
+    //             class. We are accessing its data store and we are accessing it through specifically
+    //             designed property which exposes the private thing has public. And what this means
+    //             in practice is that relationships cannot change its mind about how to store the
+    //             relationships."
+    //  We can, for instance, not change the data stire to use a dictionary instead of tuples since since
+    //  the higher level classes depends on the way the data is stored. I'm going to start using a dictionary and I'm going to remove this duplication as well or something
+    public List<(Person, Relationship, Person)> Relations => relations;
+
+    public void AddParentAndChild(Person parent, Person child)
+    {
+        relations.Add((parent, Relationship.Parent, child));
+        relations.Add((child, Relationship.Child, parent));
+    } //AddParentAndChild
+} //Relationships
+
